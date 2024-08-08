ea759911421bc943a77895c3c6969b355445d55b
Ahmad Sulaiman <ahmadsulaimansalees@gmail.com>
2024-08-08 13:28:36 +0100

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+// Hide preloader when page is loaded
+window.addEventListener("load", () => {
+  const preloader = document.querySelector(".preloader");
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 2500);
+});
